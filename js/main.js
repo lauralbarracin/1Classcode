dabc3a10510a853c7e0927b380a5a13a90fad202
lauralbarracin <97710096+lauralbarracin@users.noreply.github.com>
2022-05-06 14:40:46 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+ // this is the module pattern
+ // also called an IIFE
+ // an Inmediately Invoked Function Expression 
+
+(()=> {
+    // This is a Javascript comment
+    console.log('fired! javascript is working');
+
+    // console.log is like print("some text") in python
+})();
